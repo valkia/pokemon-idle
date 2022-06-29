c30f8508dda72cda52942b8b236d5bc0c59e94cf
dujiahao <dujiahao@flyele.net>
2022-06-30 01:15:19 +0800

    start

--- src/pages/game/index.vue
+++ src/pages/game/index.vue
@@ -8,6 +8,9 @@
     <!--    <starterCaught />-->
     <BattleList />
     <townView />
+    <button @click="start()">
+      ok
+    </button>
   </div>
 </template>
 
@@ -31,10 +34,12 @@ import { useDataStore } from '~/stores/data'
 import { init as townInit } from '~/scripts/towns/init'
 import { init as gymInit } from '~/scripts/gym/init'
 import { init as routeInit } from '~/scripts/wildBattle/init'
-import App from "~/scripts/App";
+import App from '~/scripts/App'
 const { t, locale } = useI18n()
 locale.value = 'zh-CN'
-App.start()
+const start = () => {
+  App.start()
+}
 // eslint-disable-next-line no-console
 console.log(t)
 useDataStore().setGymList(gymInit())
--- src/scripts/App.ts
+++ src/scripts/App.ts
@@ -1,17 +1,17 @@
 
-import { Game } from '~/scripts/Game'
+import type { Game } from '~/scripts/Game'
 import Multiplier from '~/modules/multiplier/Multiplier'
 import * as GameConstants from '~/enums/GameConstants'
 import LogBook from '~/modules/logbook/LogBook'
-import {Preload} from "~/scripts/utilities/Preload";
-import {Save} from "~/scripts/Save";
-import {Pokeballs} from "~/scripts/pokeballs/Pokeballs";
-import BadgeCase from "~/modules/DataStore/BadgeCase";
-import NotificationConstants from "~/modules/notifications/NotificationConstants";
-import Statistics from "~/modules/DataStore/StatisticStore";
-import Notifier from "~/modules/notifications/Notifier";
+import { Preload } from '~/scripts/utilities/Preload'
+
+import { Save } from '~/scripts/Save'
+import { Pokeballs } from '~/scripts/pokeballs/Pokeballs'
+import BadgeCase from '~/modules/DataStore/BadgeCase'
+import NotificationConstants from '~/modules/notifications/NotificationConstants'
+import Notifier from '~/modules/notifications/Notifier'
 export default class App {
-  static readonly debug = false
+  static readonly debug = true
   static game: Game
 
   static start() {
@@ -19,56 +19,56 @@ export default class App {
       Object.freeze(GameConstants)
 
     Preload.load(App.debug).then(() => {
-      //ko.options.deferUpdates = true
+      // ko.options.deferUpdates = true
 
       console.log(`[${GameConstants.formatDate(new Date())}] %cLoading Game Data..`, 'color:#8e44ad;font-weight:900;')
       // Needs to be loaded first so save data can be updated (specifically "player" data)
-      //const update = new Update()
+      // const update = new Update()
       const multiplier = new Multiplier()
 
-      player = Save.load()
-      App.game = new Game(
-        //update,
-        //new Profile(),
-        //new Breeding(multiplier),
+      // player = Save.load()
+      const player = new Pokeballs()
+      /* App.game = new Game(
+        // update,
+        // new Profile(),
+        // new Breeding(multiplier),
         new Pokeballs(),
         new Wallet(multiplier),
-        //new KeyItems(),
+        // new KeyItems(),
         new BadgeCase(),
-        //new OakItems([20, 50, 100], multiplier),
-        //new OakItemLoadouts(),
+        // new OakItems([20, 50, 100], multiplier),
+        // new OakItemLoadouts(),
         new PokemonCategories(),
         new Party(multiplier),
-        //new Shards(),
-       // new Underground(),
-        //new Farming(multiplier),
+        // new Shards(),
+        // new Underground(),
+        // new Farming(multiplier),
         new LogBook(),
-        //new RedeemableCodes(),
+        // new RedeemableCodes(),
         new Statistics(),
-        //new Quests(),
-        //new SpecialEvents(),
-        //new Discord(),
-        //new AchievementTracker(),
-        //new Challenges(),
-        //new BattleFrontier(),
+        // new Quests(),
+        // new SpecialEvents(),
+        // new Discord(),
+        // new AchievementTracker(),
+        // new Challenges(),
+        // new BattleFrontier(),
         multiplier,
-      )
-
+      ) */
       console.log(`[${GameConstants.formatDate(new Date())}] %cGame loaded`, 'color:#8e44ad;font-weight:900;')
-      Notifier.notify({ message: 'Game loaded', type: NotificationConstants.NotificationOption.info })
+      // Notifier.notify({ message: 'Game loaded', type: NotificationConstants.NotificationOption.info })
 
-      //GameController.bindToolTips()
-      //GameController.addKeyListeners()
+      // GameController.bindToolTips()
+      // GameController.addKeyListeners()
 
-     // PokedexHelper.populateFilters()
-      //PokedexHelper.updateList()
+      // PokedexHelper.populateFilters()
+      // PokedexHelper.updateList()
 
-      App.game.initialize()
+      // App.game.initialize()
 
-      //GameController.applyRouteBindings()
-      Preload.hideSplashScreen()
+      // GameController.applyRouteBindings()
+      // Preload.hideSplashScreen()
 
-      App.game.start()
+      // App.game.start()
     })
   }
 }
